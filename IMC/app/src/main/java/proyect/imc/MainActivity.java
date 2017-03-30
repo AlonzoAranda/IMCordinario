d53e66f30c341bd9760ebc2005fe2f0542924c56
joseph <josepalonzoalonzo@gmail.com>
2017-03-29 22:30:53 -0600

    main

--- IMC/app/src/main/java/proyect/imc/MainActivity.java
+++ IMC/app/src/main/java/proyect/imc/MainActivity.java
@@ -2,12 +2,65 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button btnEnviar;
+        EditText txtPeso , txtAltura;
+        ImageView img;
+        TextView lbl;
+        String s;
+        persona p = new persona();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            txtPeso = (EditText) findViewById(R.id.etPeso);
+            txtAltura = (EditText) findViewById(R.id.etAltura);
+            btnEnviar = (Button) findViewById(R.id.btnEnviar) ;
+            img = (ImageView) findViewById(R.id.imgPeso) ;
+            lbl= (TextView) findViewById(R.id.lblIMC);
+
+
+
+
+            btnEnviar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    p.setPeso(Double.parseDouble(String.valueOf(txtPeso.getText())));
+                    p.setEstatura (Double.parseDouble(String.valueOf(txtAltura.getText())));
+                    calculoIMC calculoIMC = new calculoIMC(p.getPeso() , p.getEstatura());
+                    calculoIMC.calcular();
+                    CambiarImagen(calculoIMC.resul);
+                }
+            });
+
+
+
+        }
+
+        public void CambiarImagen(double total)
+        {
+            if(total < 18.5)
+            {
+                img.setImageResource(R.drawable.bajo);
+            }
+            else if (total >18.4 & total < 25)
+            {
+                img.setImageResource(R.drawable.adecuado);
+            }
+            else if (total >= 25 & total < 30){
+                img.setImageResource(R.drawable.sobrepeso);
+            }
+            else if (total >= 30){
+                img.setImageResource(R.drawable.obeso);
+            }
+            lbl.setText("IMC: " + String.valueOf(total) ) ;
+        }
     }
-}
---
+++ IMC/app/src/main/java/proyect/imc/persona.java
@@ -0,0 +1,28 @@
+package proyect.imc;
+
+/**
+ * Created by joseph on 29/03/2017.
+ */
+
+public class persona {
+    private double peso ;
+    private double estatura;
+
+
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+}
